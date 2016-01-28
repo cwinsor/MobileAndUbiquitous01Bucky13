b90ce203d61e9acf82072999c4323919805dad23
Chris Winsor <cwinsor@gmail.com>
2016-01-28 17:22:43 -0500

    very basic RelativeLayout

--- app/src/main/java/us/cwinsor/barr5/MainActivity.java
+++ app/src/main/java/us/cwinsor/barr5/MainActivity.java
@@ -1,16 +1,61 @@
 package us.cwinsor.barr5;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        // layout
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        relativeLayout.setBackgroundColor(Color.GREEN);
+
+        // button
+        Button button = new Button(this);
+        button.setText("Click Me !!!!!!");
+        button.setBackgroundColor((Color.RED));
+
+        // username input
+        EditText username = new EditText(this);
+
+        button.setId(1);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                200,
+                r.getDisplayMetrics());
+
+        username.setWidth(px);
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        //
+        relativeLayout.addView(button, buttonDetails);
+        setContentView(relativeLayout);
     }
 
     @Override
